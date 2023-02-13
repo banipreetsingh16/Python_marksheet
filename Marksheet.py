8ca538505958e9f0977cfaa52be3eb55f043fd2d
banipreetsingh16 <123930274+banipreetsingh16@users.noreply.github.com>
2023-02-13 11:14:23 +0530

    Add files via upload

---
+++ Marksheet.py
@@ -0,0 +1,27 @@
+#VARIABLE DECLARATION (marksheet)
+ 
+id=1
+name="BANIPREET SINGH"
+marks = 88
+x = "awesome"
+print("Id =" ,id)
+print("Name ="+name)
+print("Marks =",marks)
+
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
+
+#LIST
+names = ["Vijay", "Ajay", "Aditya"]
+x,y,z= names
+print(x,y,z)
+
+import random
+
+print(random.randrange(1, 10))
+
+for x in "banana":
+  print(x)
